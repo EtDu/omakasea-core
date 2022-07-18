a242f174b7d1aeb53c639f494369b8151c936a4a
Fiaz Sami <fiaz.sami@gmail.com>
2022-07-18 11:22:46 -0500

    refactored Launcher to accept builder relative path in constructor

--- src/net/Launcher.js
+++ src/net/Launcher.js
@@ -4,11 +4,11 @@ const { fork } = require("child_process");
 const Listener = require("../redis/Listener");
 
 class Launcher {
-    constructor() {
+    constructor(builderPath) {
         console.log("LAUNCHER CREATED");
         this.listener = new Listener();
         this.listener.listen(process.env.NFT_QUEUE, (data) => {
-            const builder = fork("./src/nft/Builder.js");
+            const builder = fork(builderPath);
             builder.send(data);
         });
     }
