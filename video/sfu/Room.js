fbdd13566d346f3f845fd7c688c20c3183d0da18
Fiaz Sami <fiaz.sami@gmail.com>
2022-09-06 15:45:56 +0000

    fixed disconnect error

--- video/sfu/Room.js
+++ video/sfu/Room.js
@@ -2,107 +2,108 @@ import Worker from "./Worker.js";
 
 let WORKER;
 (async () => {
-  WORKER = await Worker.createWorker();
-  Object.freeze(WORKER);
+    WORKER = await Worker.createWorker();
+    Object.freeze(WORKER);
 })();
 
 const MEDIA_CODECS = [
-  {
-    kind: "audio",
-    mimeType: "audio/opus",
-    clockRate: 48000,
-    channels: 2,
-  },
-  {
-    kind: "video",
-    mimeType: "video/VP8",
-    clockRate: 90000,
-    parameters: {
-      "x-google-start-bitrate": 1000,
+    {
+        kind: "audio",
+        mimeType: "audio/opus",
+        clockRate: 48000,
+        channels: 2,
     },
-  },
+    {
+        kind: "video",
+        mimeType: "video/VP8",
+        clockRate: 90000,
+        parameters: {
+            "x-google-start-bitrate": 1000,
+        },
+    },
 ];
 
 function removeItems(socket, items, type) {
-  items.forEach((item) => {
-    if (item.socketId === socket.id) {
-      item[type].close();
-    }
-  });
-  items = items.filter((item) => item.socketId !== socket.id);
+    items.forEach((item) => {
+        if (item.socketId === socket.id) {
+            item[type].close();
+        }
+    });
+    items = items.filter((item) => item.socketId !== socket.id);
 
-  return items;
+    return items;
 }
 
 class Room {
-  static disconnect(globalState, socket) {
-    console.log("peer disconnected");
-    globalState.consumers = removeItems(
-      socket,
-      globalState.consumers,
-      "consumer"
-    );
-    globalState.producers = removeItems(
-      socket,
-      globalState.producers,
-      "producer"
-    );
-    globalState.transports = removeItems(
-      socket,
-      globalState.transports,
-      "transport"
-    );
+    static disconnect(globalState, socket) {
+        console.log("peer disconnected");
+        globalState.consumers = removeItems(
+            socket,
+            globalState.consumers,
+            "consumer",
+        );
+        globalState.producers = removeItems(
+            socket,
+            globalState.producers,
+            "producer",
+        );
+        globalState.transports = removeItems(
+            socket,
+            globalState.transports,
+            "transport",
+        );
 
-    const { roomName } = globalState.peers[socket.id];
-    delete globalState.peers[socket.id];
-
-    globalState.rooms[roomName] = {
-      router: globalState.rooms[roomName].router,
-      peers: globalState.rooms[roomName].peers.filter(
-        (socketId) => socketId !== socket.id
-      ),
-    };
-  }
-
-  static async create(globalState, roomName, socketId) {
-    let router;
-    let peers = [];
-    if (globalState.rooms[roomName]) {
-      router = globalState.rooms[roomName].router;
-      peers = globalState.rooms[roomName].peers || [];
-    } else {
-      router = await WORKER.createRouter({ mediaCodecs: MEDIA_CODECS });
+        if (globalState.peers[socket.id]) {
+            const { roomName } = globalState.peers[socket.id];
+            delete globalState.peers[socket.id];
+            globalState.rooms[roomName] = {
+                router: globalState.rooms[roomName].router,
+                peers: globalState.rooms[roomName].peers.filter(
+                    (socketId) => socketId !== socket.id,
+                ),
+            };
+        }
     }
 
-    console.log(`Router ID: ${router.id}`, peers.length);
+    static async create(globalState, roomName, socketId) {
+        let router;
+        let peers = [];
+        if (globalState.rooms[roomName]) {
+            router = globalState.rooms[roomName].router;
+            peers = globalState.rooms[roomName].peers || [];
+        } else {
+            router = await WORKER.createRouter({ mediaCodecs: MEDIA_CODECS });
+        }
 
-    globalState.rooms[roomName] = {
-      router: router,
-      peers: [...peers, socketId],
-    };
+        console.log(`Router ID: ${router.id}`, peers.length);
 
-    return router;
-  }
+        globalState.rooms[roomName] = {
+            router: router,
+            peers: [...peers, socketId],
+        };
 
-  static async join(globalState, socket, roomName, callback) {
-    const router = await Room.create(globalState, roomName, socket.id);
+        return router;
+    }
 
-    globalState.peers[socket.id] = {
-      socket,
-      roomName,
-      transports: [],
-      producers: [],
-      consumers: [],
-      peerDetails: {
-        name: "",
-        isAdmin: false,
-      },
-    };
+    static async join(globalState, socket, roomName, callback) {
+        const router = await Room.create(globalState, roomName, socket.id);
 
-    const rtpCapabilities = router.rtpCapabilities;
+        globalState.peers[socket.id] = {
+            socket,
+            roomName,
+            transports: [],
+            producers: [],
+            consumers: [],
+            peerDetails: {
+                name: "",
+                isAdmin: false,
+            },
+        };
 
-    callback({ rtpCapabilities });
-  }
+        const rtpCapabilities = router.rtpCapabilities;
+
+        callback({ rtpCapabilities });
+    }
 }
 
 export default Room;
