fbf349b273862d9cfb3d694a115415c6500405a0
Fiaz Sami <fiaz.sami@gmail.com>
2022-09-29 12:15:58 +0000

    resolved issue with Scheduler

--- video/rtmp/Scheduler.js
+++ video/rtmp/Scheduler.js
@@ -1,8 +1,10 @@
 const { exec } = require("node:child_process");
+const ffmpeg = require("fluent-ffmpeg");
 
 function execute(command) {
+    console.log(command);
     return new Promise((resolve, reject) => {
-        exec(command, (error, stdout, stderr) => {
+        const proc = exec(command, (error, stdout, stderr) => {
             if (error) return reject(error);
             if (stderr) return reject(stderr);
             resolve(stdout);
@@ -12,9 +14,23 @@ function execute(command) {
 
 class Scheduler {
     static play(videoPath, rtmpUrl) {
-        return execute(
-            `./omakasea-core/shell/player.sh ${videoPath} ${rtmpUrl}`,
-        );
+        return new Promise((resolve, reject) => {
+            const proc3 = new ffmpeg({ source: videoPath, timeout: 0 })
+                .addOption("-vcodec", "libx264")
+                .addOption("-acodec", "aac")
+                .addOption("-crf", 26)
+                .addOption("-f", "flv")
+                .on("start", (command) => {
+                    console.log(command);
+                })
+                .on("end", () => {
+                    resolve();
+                })
+                .output(rtmpUrl, (stdout, stderr) => {
+                    console.log("Convert complete" + stdout);
+                })
+                .run();
+        });
     }
 
     static convert(inputPath, outputPath) {
