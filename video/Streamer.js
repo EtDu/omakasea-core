e2dd53700e8a44afbba690340a8118d58daf858e
Fiaz Sami <fiaz.sami@gmail.com>
2022-11-10 21:02:30 +0000

    moved file deletion to parent

--- video/Streamer.js
+++ video/Streamer.js
@@ -5,30 +5,20 @@ const RTMP_URL = `${process.env.RELAY_RTMP_URL}=${process.env.STREAM_KEY}`;
 
 import Client from "../http/Client.js";
 import FFMPEG from "../video/FFMPEG.js";
-import FileSystem from "../util/FileSystem.js";
 
 const BROADCASTER_HOST = process.env.BROADCASTER_HOST;
 const BROADCASTER_PORT = process.env.BROADCASTER_PORT;
 const BROADCASTER_URL = `http://${BROADCASTER_HOST}:${BROADCASTER_PORT}`;
 
-const ERROR_DELAY = 60 * 1000;
+const ERROR_DELAY = 5000;
 const STREAM_DELAY = 5000;
 
-function remove(video) {
-    if (FileSystem.exists(video.playing)) {
-        console.log(`D - ${video.uuid} - ${BROADCASTER_URL}/next`);
-        FileSystem.delete(video.playing);
-    }
-}
-
 class Streamer {
-    static next(data) {
+    static cleanup(data) {
         return new Promise((resolve, reject) => {
             setTimeout(() => {
                 Client.post(`${BROADCASTER_URL}/next`, { data: data })
-                    .then(() => {
-                        remove(data);
-                    })
+                    .then(resolve)
                     .catch(() => {
                         console.log(
                             `\nPLAYER IS DOWN WAITING ${ERROR_DELAY / 1000}s`,
@@ -42,15 +32,15 @@ class Streamer {
 
     static start(video) {
         return new Promise((resolve, reject) => {
-            console.log(`P > ${video.uuid}`);
+            console.log(`S > ${video.uuid}`);
             FFMPEG.stream(video.playing, RTMP_URL)
                 .then(() => {
-                    Streamer.next(video).then(resolve);
+                    Streamer.cleanup(video).then(resolve);
                 })
                 .catch(() => {
                     console.log(`S * ${video.uuid}`);
                     setTimeout(() => {
-                        Streamer.next(video).then(resolve);
+                        Streamer.cleanup(video).then(resolve);
                     }, ERROR_DELAY);
                 });
         });
