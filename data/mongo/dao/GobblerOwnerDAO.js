ebbae544f7ca26fa46d94f4c46088950f0043bc4
Fiaz Sami <fiaz.sami@gmail.com>
2022-12-08 04:30:33 +0000

    read through signature

--- data/mongo/dao/GobblerOwnerDAO.js
+++ data/mongo/dao/GobblerOwnerDAO.js
@@ -1,10 +1,28 @@
 import crypto from "crypto";
 import ShortHash from "short-hash";
 
+import ethers from "ethers";
+const getAddress = ethers.utils.getAddress;
+
 import __BaseDAO__ from "./__BaseDAO__.js";
 import GobblerOwner from "../models/GobblerOwner.js";
 
 class GobblerOwnerDAO {
+    static readSignature(req) {
+        return new Promise((resolve, reject) => {
+            const sig = req.body.sig;
+            const message = JSON.parse(req.body.message);
+            const address = getAddress(
+                recoverPersonalSignature({
+                    data: message,
+                    signature: sig,
+                }),
+            );
+
+            resolve({ address, message });
+        });
+    }
+
     static get(query) {
         return new Promise((resolve, reject) => {
             __BaseDAO__.__get__(GobblerOwner, query).then((document) => {
@@ -21,18 +39,20 @@ class GobblerOwnerDAO {
         return __BaseDAO__.__search__(GobblerOwner, query, fields);
     }
 
-    static create() {
+    static createLinks(address) {
         return new Promise((resolve, reject) => {
             const createdAt = Date.now();
             const links = {
                 naughty: {
                     side: "naughty",
                     inviteID: ShortHash(crypto.randomUUID()),
+                    originator: address,
                     createdAt,
                 },
                 nice: {
                     side: "nice",
                     inviteID: ShortHash(crypto.randomUUID()),
+                    originator: address,
                     createdAt,
                 },
             };
--- data/mongo/schemas/GobblerOwnerSchema.js
+++ data/mongo/schemas/GobblerOwnerSchema.js
@@ -1,7 +1,8 @@
 export default {
     side: { type: String },
     inviteID: { type: String },
-    address: { type: String, default: null },
-    twitter: { type: String, default: null },
+    originator: { type: String, default: null },
+    owner: { type: String, default: null },
+    twitterHandle: { type: String, default: null },
     createdAt: { type: Number },
 };
