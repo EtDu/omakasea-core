facd784b9dfbaae01335872bbe89becb26d43b72
Fiaz Sami <fiaz.sami@gmail.com>
2022-12-20 04:32:57 +0000

    signed message against front-end

--- util/SignGobbler.js
+++ util/SignGobbler.js
@@ -3,49 +3,56 @@ dotenv.config();
 
 import utils from "ethers/lib/utils.js";
 import ethers from "ethers";
+import { recoverPersonalSignature } from "@metamask/eth-sig-util";
 
 import { ABI } from "../../../blockchain/EthGobblersABI.js";
 
 const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
-const RPC_URL = process.env.RPC_URL;
+const HTTP_RPC_URL = process.env.HTTP_RPC_URL;
 const BLOCKCHAIN_NETWORK = process.env.BLOCKCHAIN_NETWORK;
 
-const PROVIDER = new ethers.providers.JsonRpcProvider(
-    RPC_URL,
+const HTTP_PROVIDER = new ethers.providers.JsonRpcProvider(
+    HTTP_RPC_URL,
     BLOCKCHAIN_NETWORK,
 );
 
+const ETHERS_CONTRACT = new ethers.Contract(CONTRACT_ADDRESS, ABI, PROVIDER);
+
 class SignGobbler {
-    static sign(fnName, senderAddress) {
+    static listen() {
+        const contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, PROVIDER);
+
+        contract.on("Broadcast", (event) => {
+            console.log(event);
+        });
+    }
+
+    static sign(fnName, signature) {
         return new Promise((resolve, reject) => {
-            const contract = new ethers.Contract(
-                CONTRACT_ADDRESS,
-                ABI,
-                PROVIDER,
+            const senderAddress = getAddress(
+                recoverPersonalSignature(signature),
             );
 
-            contract.signatureNonce(senderAddress).then((res) => {
+            ETHERS_CONTRACT.signatureNonce(senderAddress).then((res) => {
                 const sigNonce = res._hex;
                 const fnNameSig = Number(
                     utils.keccak256(utils.toUtf8Bytes(fnName)).substring(0, 10),
                 );
 
-                contract
-                    .hashMessage(
-                        senderAddress,
-                        CONTRACT_ADDRESS,
-                        fnNameSig,
-                        sigNonce,
-                    )
-                    .then((messageHash) => {
-                        const SIGNER = new ethers.Wallet(
-                            process.env.PRIVATE_KEY,
-                            PROVIDER,
-                        );
-                        SIGNER.signMessage(messageHash).then((signature) => {
-                            resolve(signature);
-                        });
+                ETHERS_CONTRACT.hashMessage(
+                    senderAddress,
+                    CONTRACT_ADDRESS,
+                    fnNameSig,
+                    sigNonce,
+                ).then((messageHash) => {
+                    const SIGNER = new ethers.Wallet(
+                        process.env.PRIVATE_KEY,
+                        HTTP_PROVIDER,
+                    );
+                    SIGNER.signMessage(messageHash).then((signature) => {
+                        resolve(signature);
                     });
+                });
             });
         });
     }
