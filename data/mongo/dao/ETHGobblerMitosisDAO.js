126db2d6b489e144e83bb98bdd5a39dbf37e4c97
Fiaz Sami <fiaz.sami@gmail.com>
2022-12-31 13:22:47 -0600

    fetch for mitosis DAO

--- data/mongo/dao/ETHGobblerMitosisDAO.js
+++ data/mongo/dao/ETHGobblerMitosisDAO.js
@@ -18,6 +18,26 @@ class ETHGobblerMitosisDAO {
     static save(doc) {
         return __BaseDAO__.__save__(doc);
     }
+
+    static fetch(query) {
+        return new Promise((resolve, reject) => {
+            __BaseDAO__
+                .__search__(
+                    ETHGobblerMitosis,
+                    query,
+                    {},
+                    { tokenID: 1, createdAt: 1 },
+                )
+                .then((results) => {
+                    if (results.length > 0) {
+                        resolve(results[0]);
+                    } else {
+                        reject();
+                    }
+                })
+                .catch(reject);
+        });
+    }
 }
 
 export default ETHGobblerMitosisDAO;
