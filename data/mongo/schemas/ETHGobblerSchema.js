c670d40c2339406df4af5a4761a265885eeddbdb
Fiaz Sami <fiaz.sami@gmail.com>
2023-01-11 01:18:08 +0000

    changed sleptAt default to null

--- data/mongo/schemas/ETHGobblerSchema.js
+++ data/mongo/schemas/ETHGobblerSchema.js
@@ -6,7 +6,7 @@ export default {
     health: { type: Number, default: 100 },
     mitosisCount: { type: Number, default: 0 },
     traitCount: { type: Number, default: 0 },
-    sleptAt: { type: Number, default: 0 },
+    sleptAt: { type: Number, default: null },
     generation: { type: Number, default: null },
     isAwake: { type: Boolean, default: true },
     isBuried: { type: Boolean, default: false },
