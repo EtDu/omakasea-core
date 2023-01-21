481ec529e0cdb0ac25f558bf38034483cbce8495
Fiaz Sami <fiaz.sami@gmail.com>
2023-01-21 00:51:16 +0000

    added random image assignment

--- data/mongo/dao/ETHGobblerImageDAO.js
+++ data/mongo/dao/ETHGobblerImageDAO.js
@@ -1,7 +1,23 @@
 import __BaseDAO__ from "./__BaseDAO__.js";
 import ETHGobblerImage from "../models/ETHGobblerImage.js";
 
+function random(max) {
+    return Math.floor(Math.random() * max);
+}
+
 class ETHGobblerImageDAO {
+    static async assignRandom(gobbler, subDir = null) {
+        const { generation } = gobbler;
+        const images = this.search({ tokenID: null, generation, subDir });
+        const selectedImage = images[random(images.length)];
+        selectedImage.tokenID = gobbler;
+        await this.save(selectedImage);
+    }
+
+    static async count(query) {
+        return ETHGobblerImage.countDocuments(query).exec();
+    }
+
     static get(query = {}) {
         return __BaseDAO__.__get__(ETHGobblerImage, query);
     }
